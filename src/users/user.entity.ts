ef645d7f300f914370ceacab4fa9dc75be524df6
Daniel Rashba <danirdd92@gmail.com>
2021-10-31 06:41:34 +0200

    Added Reports Dtos, configured one to many relationship with users

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,3 +1,5 @@
+import { report } from 'process';
+import { Report } from 'src/reports/report.entity';
 import {
   AfterInsert,
   Entity,
@@ -5,6 +7,7 @@ import {
   PrimaryGeneratedColumn,
   AfterUpdate,
   AfterRemove,
+  OneToMany,
 } from 'typeorm';
 @Entity()
 export class User {
@@ -16,6 +19,13 @@ export class User {
 
   @Column()
   password: string;
+
+  //TODO - CHANGE IT
+  @Column({ default: true })
+  admin: boolean;
+
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
 
   @AfterInsert()
   logInsert() {
