81f3c01f8ef739c634a2ea1562d90bd67801353b
Daniel Rashba <danirdd92@gmail.com>
2021-11-01 09:26:23 +0200

    fixed e2e testing not finding entities, added prod migration profile for heroku

--- ormconfig.js
+++ ormconfig.js
@@ -18,11 +18,20 @@ switch (process.env.NODE_ENV) {
     Object.assign(dbConfig, {
       type: 'sqlite',
       database: 'test.sqlite',
-      entities: ['**/*.entity.tes'],
+      entities: ['**/*.entity.ts'],
       migrationsRun: true,
     });
     break;
   case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
     break;
 
   default:
